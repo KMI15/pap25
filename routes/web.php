4934842b916ccaac8cb32654994fc937b44a69f7
Gonçalo Veiga <veigaveiga50@gmail.com>
2024-11-18 14:18:28 +0000

    atualizado

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
+use App\Http\Controllers\RestaurantController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +13,9 @@ Route::get('/categories', function () {
     $categories = Category::all();    
     return view('categories.index', compact('categories'));
 });
+
+Route::get('/', [RestaurantController::class, 'index']);
+
 
 Route::middleware([
     'auth:sanctum',
