8e7f928fdca2f46aa586a6865e6994302578bcb4
Gonçalo Veiga <veigaveiga50@gmail.com>
2025-02-17 14:26:26 +0000

    Header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,68 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+<x-guestLayout>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portal de Volanturiado Juvenil</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f8f9fa;
-            color: #333;
-        }
-
-        header {
-            background-color: #ff6347;
-            color: white;
-            padding: 20px;
-            text-align: center;
-        }
-
-        nav ul {
-            list-style-type: none;
-            padding: 0;
-        }
-
-        nav ul li {
-            display: inline;
-            margin: 0 15px;
-        }
-
-        nav ul li a {
-            color: white;
-            text-decoration: none;
-        }
-
-        main {
-            padding: 20px;
-        }
-
-        footer {
-            background-color: #343a40;
-            color: white;
-            text-align: center;
-            padding: 10px 0;
-            position: relative;
-            bottom: 0;
-            width: 100%;
-        }
-    </style>
+    
 </head>
 <body>
-    <header>
-        <h1>Bem-vindo ao Portal</h1>
-        <nav>
-            <ul>
-                <li><a href="#home">Início</a></li>
-                <li><a href="#menu">Menu</a></li>
-                <li><a href="#contact">Contato</a></li>
-                <li><a href="login">Login</a></li>
-                <li><a href="register">Register</a></li>
-            </ul>
-        </nav>
-    </header>
+    
 
     <main>
         <section id="home">
@@ -76,4 +22,6 @@
         </div>
     </footer>
 </body>
+</x-guestLayout>
+
 </html>
